941d7cf8a2c7c9545f18bd029378e529cbc27f8d
Terry Chia <chiawz@hotmail.com>
2023-07-23 10:47:00 +0800

    feat: add login page

---
+++ ev-frontend/src/app/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {Amplify} from 'aws-amplify';
+import awsExports from "./aws-exports";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Auth } from "aws-amplify";
+
+// Configure Amplify in index file or root file
+Amplify.configure({
+  Auth: {
+    region: awsExports.REGION,
+    userPoolId: awsExports.USER_POOL_ID,
+    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
+  }
+})
+
+function App() {
+  return (
+      <Authenticator signUpAttributes={['email']}>
+        {({ signOut, user }) => (
+            <div>
+              <p>Welcome {user.username}</p>
+              <button onClick={signOut}>Sign out</button>
+            </div>
+        )}
+      </Authenticator>
+  );
+}
+
+export default App;
