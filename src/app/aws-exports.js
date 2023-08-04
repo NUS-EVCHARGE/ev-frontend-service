04155c45191dbb453b7efc8a83ea6da87c44a6a6
Terry Chia <chiawz@hotmail.com>
2023-08-04 08:18:43 +0800

    chore: update aws cognito setup

--- src/app/aws-exports.js
+++ src/app/aws-exports.js
@@ -1,6 +1,6 @@
 // replace the user pool region, id, and app client id details
 export default {
     "REGION": "ap-southeast-1",
-    "USER_POOL_ID": "ap-southeast-1_Ul6p1U2nv",
-    "USER_POOL_APP_CLIENT_ID": "4kg0l84cjf874dsiudmstq5ig2"
+    "USER_POOL_ID": "ap-southeast-1_ZIUwOvJUt",
+    "USER_POOL_APP_CLIENT_ID": "77f40bm680p51j49ta30ogd3ad"
 }
