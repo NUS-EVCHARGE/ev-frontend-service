e09b903b0f85cab74fc7d6e79af79dbc5d315c7b
tkpython <lawtekai@gmail.com>
2023-09-25 00:48:59 +0800

    [WIP] new charger form

--- src/app/admin/chargerDetails.tsx
+++ src/app/admin/chargerDetails.tsx
@@ -1,8 +1,19 @@
 'use client'
 import { useEffect, useState } from "react";
 import { Card, Button, List, Avatar } from "antd"
+// import { AddCharger } from "./addCharger";
+import { NewPage } from "./newPage";
+// import { newCharger } from "./new_charger"
+import { useRouter } from 'next/navigation';
 
 function ChargerDetails({ user }: any) {
+
+    const router = useRouter();
+
+    const navigateToAddChargerPage = () => {
+        router.push('/new_charger');
+    };
+
     const data = [
         {
             title: 'Ant Design Title 1',
@@ -18,9 +29,9 @@ function ChargerDetails({ user }: any) {
         },
     ];
     return (
-        <Card type="inner" title="Chargers" extra={<Button>
-            Add Charger
-        </Button>}>
+        <Card type="inner" title="Chargers" 
+        extra={<Button onClick={navigateToAddChargerPage}>Add Charger</Button>}>
+
             <List
                 pagination={{ position: 'bottom', align: 'center' }}
                 dataSource={data}
