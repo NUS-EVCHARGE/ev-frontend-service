08902c6a64ab05255f3a646086990d682436f818
Terry Chia <chiawz@hotmail.com>
2023-10-10 20:23:39 +0800

    feat: getAllBookings

--- src/app/bill/page.tsx
+++ src/app/bill/page.tsx
@@ -1,32 +1,55 @@
 
 'use client'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Avatar, Button, List, Radio, Space } from 'antd';
-import { get } from 'http';
 import { useRouter } from 'next/navigation';
-
-const data = [
-    {
-        title: 'Ant Design Title 1',
-    },
-    {
-        title: 'Ant Design Title 2',
-    },
-    {
-        title: 'Ant Design Title 3',
-    },
-    {
-        title: 'Ant Design Title 4',
-    },
-];
+import axios, { AxiosRequestHeaders } from 'axios';
+import { getJwtToken } from '../utils';
 
 const billOption = ['outstanding', 'completed'];
 // const defaultBookingOption = 'upcoming booking'
 
 function Bill() {
-    const [billList, setBillList] = useState()
+    const [billList, setBillList] = useState<Booking[]>([])
     const [currentBillOption, setBillOptions] = useState('outstanding')
     const router = useRouter()
+
+    type Booking = {
+        id: number,
+        charger_id: number,
+        Email: string,
+        start_time: string,
+        end_time: string,
+        Status: string,
+    }
+
+    useEffect(() => {
+        async function getAllBooking() {
+            const jwtToken = await getJwtToken()
+            const headers: AxiosRequestHeaders = {
+                'Content-Type': 'application/json',
+              };
+              if (jwtToken) {
+                headers['Authentication'] = jwtToken;
+              }
+            axios.get(process.env.NEXT_PUBLIC_REACT_APP_BASE_URL + '/booking', {
+                headers: headers
+            }).then((res) => {
+                const data : Booking[] = res.data.filter((item: Booking) => {
+                    if (item.Status == "completed") {
+                        return {
+                            item
+                        }
+                    }
+                });
+                setBillList(data);
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+        getAllBooking()
+    }, [])
+
     return (
         <>
             <Space direction="vertical" style={{ marginBottom: '20px' }} size="middle">
@@ -49,15 +72,15 @@ function Bill() {
             </Space>
             <List
                 pagination={{ position: 'bottom', align: 'center' }}
-                dataSource={data}
+                dataSource={billList}
                 renderItem={(item, index) => (
                     <List.Item>
                         <List.Item.Meta
                             avatar={
                                 <Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />
                             }
-                            title={<a href="https://ant.design">{item.title}</a>}
-                            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                            title={<a href="https://ant.design">Booking: {item.id} - Charger: {item.charger_id}</a>}
+                            description={`Start time: ${item.start_time} - End time: ${item.end_time}`}
                         />
                         {currentBillOption == "outstanding" ? <Button onClick={() => {
                             router.push("/bill/payment")
