54459d59f0df0a4c4971af612bf672964550f335
Terry Chia <chiawz@hotmail.com>
2023-10-10 20:23:39 +0800

    chore: move to env variable (+8 squashed commits) Squashed commits: [3c59244] chore: revert back yarn.lock file [4d86b3a] chore: remove import statement clean up [4f0c0c5] fix: able to npm build [d30478e] fix: UI Payments [7c261a2] feat: user payment page [bfa195e] feat: bill page [11b62e6] feat: display complete and pending transaction [08902c6] feat: getAllBookings

--- src/app/bill/page.tsx
+++ src/app/bill/page.tsx
@@ -1,32 +1,73 @@
 
 'use client'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Avatar, Button, List, Radio, Space } from 'antd';
-import { get } from 'http';
 import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import { getJwtToken } from '../utils';
 
-const data = [
-    {
-        title: 'Ant Design Title 1',
-    },
-    {
-        title: 'Ant Design Title 2',
-    },
-    {
-        title: 'Ant Design Title 3',
-    },
-    {
-        title: 'Ant Design Title 4',
-    },
-];
+const billOption = ['outstanding', 'completed'];    
 
-const billOption = ['outstanding', 'completed'];
-// const defaultBookingOption = 'upcoming booking'
+type UserPaymentItem = {
+    id: number,
+    charger_id: number,
+    chargerAddress : string,
+    TotalBill: number,
+    FinalBill: number,
+    Coupon: string,
+    Email: string,
+    start_time: string,
+    end_time: string,
+    Status: string,
+    normalRate: number,
+    bookingId: number,
+    UserEmail: string,
+    paymentStatus: string
+}
 
 function Bill() {
-    const [billList, setBillList] = useState()
+    type billInfosType = Record<string, UserPaymentItem[]>
+    const [billList, setBillList] = useState<billInfosType>()
     const [currentBillOption, setBillOptions] = useState('outstanding')
     const router = useRouter()
+
+    function getFilteredBillList() {
+        if (!billList) {
+            return []
+        }
+        if (currentBillOption == 'outstanding') {
+            const pendingList = billList['pending']
+            if (pendingList) {
+                return pendingList.filter((item) => item.paymentStatus == 'pending')
+            }
+        } else {
+            const pendingList = billList['completed']
+            if (pendingList) {
+                return pendingList.filter((item) => item.paymentStatus == 'completed')
+            }
+        }
+    }
+
+    useEffect(() => {
+
+        async function getAllUserPayment() {
+            const jwtToken = await getJwtToken()
+            axios.get(process.env.NEXT_PUBLIC_REACT_APP_BASE_URL + '/payment/user/getAllBooking', {
+                headers: {
+                    Accept: 'application/json',
+                    Authentication: jwtToken?.toString()
+                }
+            }).then((res) => {
+                const data: billInfosType = res.data
+                setBillList(data);
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+
+        getAllUserPayment()
+    },[]);
+
     return (
         <>
             <Space direction="vertical" style={{ marginBottom: '20px' }} size="middle">
@@ -49,15 +90,20 @@ function Bill() {
             </Space>
             <List
                 pagination={{ position: 'bottom', align: 'center' }}
-                dataSource={data}
+                dataSource={getFilteredBillList()}
                 renderItem={(item, index) => (
                     <List.Item>
                         <List.Item.Meta
                             avatar={
                                 <Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />
                             }
-                            title={<a href="https://ant.design">{item.title}</a>}
-                            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                            title={<a href="https://ant.design">Bookng Number: {item.id}
+                            <br/> Total Bill: ${item.TotalBill}</a>}    
+                            description={
+                            <a>Start time: {new Date(item.start_time).toLocaleString("en-SG")} - End time: {new Date(item.end_time).toLocaleString("en-SG")}
+                            <br/>Location: {item.chargerAddress}
+                            </a>
+                            }
                         />
                         {currentBillOption == "outstanding" ? <Button onClick={() => {
                             router.push("/bill/payment")
@@ -73,3 +119,4 @@ function Bill() {
     );
 }
 export default Bill
+export type { UserPaymentItem }
