aac40ab4b6ca9b8ada0c2d03051c03fb7da9a3c6
tkpython <lawtekai@gmail.com>
2023-10-15 12:31:02 +0800

    [WIP] Add Charger, Edit and Delete func

--- src/app/admin/chargerTable.tsx
+++ src/app/admin/chargerTable.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Radio, Space, Table, Tag } from 'antd';
+import { Button, Radio, Space, Table, Tag, Popconfirm } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import jwt from 'jsonwebtoken';
 import { getJwtToken } from '../utils/index';
@@ -17,7 +17,7 @@ interface DataType {
 }
 
 interface RatesType {
-    key: string,
+    key: React.Key,
     chargerId: string,
     address: string,
     normalRate: number,
@@ -48,65 +48,135 @@ const bottomOptions = [
   { label: 'none', value: 'none' },
 ];
 
-const columns: ColumnsType<DataType> = [
-  {
-    title: 'Charger ID',
-    dataIndex: 'chargerId',
-    key: 'chargerId',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Normal Rate',
-    dataIndex: 'normalRate',
-    key: 'normalRate',
-    render: (text) => <a>{text.toFixed(2)}</a>,
-  },
-  {
-    title: 'Penalty Rate',
-    dataIndex: 'penaltyRate',
-    key: 'penaltyRate',
-    render: (text) => <a>{text.toFixed(2)}</a>,
-  },
-  {
-    title: 'No Show Rate',
-    dataIndex: 'noShowRate',
-    key: 'noShowRate',
-    render: (text) => <a>{text.toFixed(2)}</a>,
-  },
-  {
-    title: 'Status',
-    key: 'status',
-    dataIndex: 'status',
-    render: (status) => (
-      <span>
-        {(() => {
-            console.log("status", status);
-          if (status == 'active'){
-              return (
-                    <Tag color='green' key={status}>{status.toUpperCase()}</Tag>
-              )
-          }
-          else {
-                return (
-                    <Tag color='red' key={status}>{status.toUpperCase()}</Tag>
-                )
-          }
-          
-        })()}
-      </span>
-    ),
-  },
 
-];
+
 
 function ChargersList() {
   const [chargers, setChargers] = useState<RatesType[]>();
+  const [count, setCount] = useState(2);
   const [rates, setRates] = useState<RatesType[]>();
+  const [dataSource, setDataSource] = useState<RatesType[]>([
+    {
+      key: '1',
+      chargerId: '1',
+      address: 'New York No. 1 Lake Park',
+      normalRate: 1.00,
+      penaltyRate: 2.00,
+      noShowRate: 3.00,
+      status: 'active',
+    },
+    {
+        key: '2',
+        chargerId: '2',
+        address: 'London No. 1 Lake Park',
+        normalRate: 1.00,
+        penaltyRate: 2.00,
+        noShowRate: 3.00,
+        status: 'active',
+    },
+    {
+        key: '3',
+        chargerId: '3',
+        address: 'Sydney No. 1 Lake Park',
+        normalRate: 1.00,
+        penaltyRate: 2.00,
+        noShowRate: 3.00,
+        status: 'inactive',
+    },]
+  );
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Charger ID',
+      dataIndex: 'chargerId',
+      key: 'chargerId',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Normal Rate',
+      dataIndex: 'normalRate',
+      key: 'normalRate',
+      render: (text) => <a>{text.toFixed(2)}</a>,
+    },
+    {
+      title: 'Penalty Rate',
+      dataIndex: 'penaltyRate',
+      key: 'penaltyRate',
+      render: (text) => <a>{text.toFixed(2)}</a>,
+    },
+    {
+      title: 'No Show Rate',
+      dataIndex: 'noShowRate',
+      key: 'noShowRate',
+      render: (text) => <a>{text.toFixed(2)}</a>,
+    },
+    {
+      title: 'Status',
+      key: 'status',
+      dataIndex: 'status',
+      render: (status) => (
+        <span>
+          {(() => {
+              console.log("status", status);
+            if (status == 'active'){
+                return (
+                      <Tag color='green' key={status}>{status.toUpperCase()}</Tag>
+                )
+            }
+            else {
+                  return (
+                      <Tag color='red' key={status}>{status.toUpperCase()}</Tag>
+                  )
+            }
+            
+          })()}
+        </span>
+      ),
+    },
+    {
+      title: 'Operation',
+      dataIndex: 'operation',
+      render: (_, record: { key: React.Key }) =>
+          dataSource.length >= 1 ? (
+            <div>
+              <div>
+                <Popconfirm title="Sure to edit?" onConfirm={() => handleDelete(record.key)}>
+                  <a>Edit</a>
+                </Popconfirm>
+              </div>
+              <div>
+                <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
+                  <a>Delete</a>
+                </Popconfirm>
+              </div>
+            </div>
+          ) : null,
+      },
+  
+  ];
+  const handleAdd = () => {
+    const newData: RatesType = {
+      key: count,
+      chargerId: `${count}`,
+      address: `London, Park Lane no. ${count}`,
+      normalRate: 1.1,
+      penaltyRate: 1.3,
+      noShowRate: 1.5,
+      status: 'Active',
+      
+    };
+    setDataSource([...dataSource, newData]);
+    setCount(count + 1);
+  };
+
+  const handleDelete = (key: React.Key) => {
+    const newData = dataSource.filter((item) => item.key !== key);
+    setDataSource(newData);
+  };
 
   // Get Charger details for provider
   async function GetChargers() {
@@ -138,14 +208,15 @@ function ChargersList() {
         // setRates(found); 
         // return found;
         console.log("TK1")
-        console.log("data[id]", Object.keys(0))
+        // console.log("data[id]", Object.keys(0))
         // return data[id];
         // setRates(data);
         return rates;
       }
         // Add the charger details to the array
         console.log("item.rates_id", item.rates_id)
-        let rates = GetRates(item.rates_id)
+        // let rates = GetRates(item.rates_id)
+        
         console.log("GET rates", rates)
         newArr.push({
           key: '1',
@@ -160,7 +231,7 @@ function ChargersList() {
     // const data : DataType[] = newArr;
     const chargerList = GetChargers()
     return newArr;
-    setChargers(newArr);
+    // setChargers(newArr);
   }
   const [top, setTop] = useState<TablePaginationPosition>('topCenter');
   const [bottom, setBottom] = useState<TablePaginationPosition>('bottomCenter');
@@ -174,7 +245,9 @@ function ChargersList() {
     }, chargers);
   return (
     <div>
-      <Table columns={columns} pagination={{ position: [bottom] }} dataSource={chargers} />
+    <Button onClick={handleAdd} style={{ marginBottom: 16 }}>Add a Charger</Button>
+    
+      <Table columns={columns} pagination={{ position: [bottom] }} dataSource={dataSource} />
     </div>
   )
 }
