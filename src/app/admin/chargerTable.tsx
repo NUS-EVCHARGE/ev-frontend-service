0198c95a67433ba415a4ac8d5b044e3570ad8df2
tkpython <lawtekai@gmail.com>
2023-10-17 00:52:21 +0800

    [WIP] Edit,save,cancel

--- src/app/admin/chargerTable.tsx
+++ src/app/admin/chargerTable.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Radio, Space, Table, Tag, Popconfirm } from 'antd';
+import { Select, Form, InputNumber, Input, Button, Radio, Space, Table, Tag, Popconfirm, Typography } from 'antd';
+import type { MenuProps } from 'antd';
+import { Dropdown, message, Tooltip } from 'antd';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+
 import type { ColumnsType } from 'antd/es/table';
 import jwt from 'jsonwebtoken';
 import { getJwtToken } from '../utils/index';
@@ -48,13 +52,60 @@ const bottomOptions = [
   { label: 'none', value: 'none' },
 ];
 
+interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
+  editing: boolean;
+  dataIndex: string;
+  title: any;
+  inputType: 'number' | 'text';
+  record: RatesType;
+  index: number;
+  // children: React.ReactNode;
+}
 
+const EditableCell: React.FC<EditableCellProps> = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
 
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{ margin: 0 }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
 
 function ChargersList() {
   const [chargers, setChargers] = useState<RatesType[]>();
   const [count, setCount] = useState(2);
   const [rates, setRates] = useState<RatesType[]>();
+  const [editingKey, setEditingKey] = useState('');
+  const [form] = Form.useForm();
+  
+
+
+  const isEditing = (record: RatesType) => record.key === editingKey;
   const [dataSource, setDataSource] = useState<RatesType[]>([
     {
       key: '1',
@@ -84,43 +135,121 @@ function ChargersList() {
         status: 'inactive',
     },]
   );
-  const columns: ColumnsType<DataType> = [
+
+  const edit = (record: Partial<RatesType> & { key: React.Key }) => {
+    console.log('EDIT TEST', record)
+    console.log('EDIT TEST')
+    form.setFieldsValue({ chargerId: '', age: '', address: '', ...record });
+    setEditingKey(record.key);
+  };
+
+  const cancel = () => {
+    setEditingKey('');
+  };
+
+  const save = async (key: React.Key) => {
+    try {
+      const row = (await form.validateFields()) as RatesType;
+
+      const newData = [...dataSource];
+      const index = newData.findIndex((item) => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        setDataSource(newData);
+        setEditingKey('');
+      } else {
+        newData.push(row);
+        setDataSource(newData);
+        setEditingKey('');
+      }
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    message.info('Click on menu item.');
+    console.log('click', e);
+  };
+  const dropdownitems: MenuProps['items'] = [
+    {
+      label: 'Active',
+      key: 'active',
+      icon: <UserOutlined />,
+    },
+    {
+      label: 'Inactive',
+      key: 'inactive',
+      icon: <UserOutlined />,
+    },
+  ];
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+  const menuProps = {
+    dropdownitems,
+    onClick: handleMenuClick,
+  };
+
+  //: ColumnsType<DataType>
+  const columns = [
     {
       title: 'Charger ID',
       dataIndex: 'chargerId',
       key: 'chargerId',
-      render: (text) => <a>{text}</a>,
+      render: (text: string) => <a>{text}</a>,
     },
     {
       title: 'Address',
       dataIndex: 'address',
       key: 'address',
+      editable: true,
     },
     {
       title: 'Normal Rate',
       dataIndex: 'normalRate',
       key: 'normalRate',
-      render: (text) => <a>{text.toFixed(2)}</a>,
+      editable: true,
+      render: (text: any) => <a>{parseFloat(text).toFixed(2)}</a>,
     },
     {
       title: 'Penalty Rate',
       dataIndex: 'penaltyRate',
       key: 'penaltyRate',
-      render: (text) => <a>{text.toFixed(2)}</a>,
+      editable: true,
+      render: (text: any) => <a>{parseFloat(text).toFixed(2)}</a>,
     },
     {
       title: 'No Show Rate',
       dataIndex: 'noShowRate',
       key: 'noShowRate',
-      render: (text) => <a>{text.toFixed(2)}</a>,
+      editable: true,
+      render: (text: any) => <a>{parseFloat(text).toFixed(2)}</a>,
     },
     {
       title: 'Status',
       key: 'status',
       dataIndex: 'status',
-      render: (status) => (
+      render: (status: string) => (
         <span>
-          {(() => {
+          <Select
+            defaultValue="disabled"
+            style={{ 
+              width: 120,
+              color: status == 'active' ? 'green' : 'red',
+            }}
+            onChange={handleChange}
+            disabled={true}
+            options={[
+              { value: 'active', label: 'Active' },
+              { value: 'disabled', label: 'Disabled' },
+            ]}
+          />
+          {/* {(() => {
               console.log("status", status);
             if (status == 'active'){
                 return (
@@ -133,29 +262,47 @@ function ChargersList() {
                   )
             }
             
-          })()}
+          })()} */}
         </span>
       ),
     },
     {
       title: 'Operation',
       dataIndex: 'operation',
-      render: (_, record: { key: React.Key }) =>
-          dataSource.length >= 1 ? (
-            <div>
-              <div>
-                <Popconfirm title="Sure to edit?" onConfirm={() => handleDelete(record.key)}>
-                  <a>Edit</a>
-                </Popconfirm>
-              </div>
-              <div>
-                <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
-                  <a>Delete</a>
-                </Popconfirm>
-              </div>
-            </div>
-          ) : null,
+      render: (_: any, record: RatesType) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <Typography.Link onClick={() => save(record.key)} style={{ marginRight: 8 }}>
+              Save
+            </Typography.Link>
+            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+            Edit
+          </Typography.Link>
+        );
       },
+    },
+      // render: (_, record: { key: React.Key }) =>
+      //     dataSource.length >= 1 ? (
+      //       <div>
+      //         <div>
+      //           <Popconfirm title="Sure to edit?" onConfirm={() => handleDelete(record.key)}>
+      //             <a>Edit</a>
+      //           </Popconfirm>
+      //         </div>
+      //         <div>
+      //           <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
+      //             <a>Delete</a>
+      //           </Popconfirm>
+      //         </div>
+      //       </div>
+      //     ) : null,
+      // },
   
   ];
   const handleAdd = () => {
@@ -163,9 +310,9 @@ function ChargersList() {
       key: count,
       chargerId: `${count}`,
       address: `London, Park Lane no. ${count}`,
-      normalRate: 1.1,
-      penaltyRate: 1.3,
-      noShowRate: 1.5,
+      normalRate: 0.0,
+      penaltyRate: 0.0,
+      noShowRate: 0.0,
       status: 'Active',
       
     };
@@ -178,6 +325,7 @@ function ChargersList() {
     setDataSource(newData);
   };
 
+  
   // Get Charger details for provider
   async function GetChargers() {
     const chargerUrl = String(process.env.NEXT_PUBLIC_REACT_APP_BASE_URL) + "provider/1/charger"
@@ -243,11 +391,46 @@ function ChargersList() {
       // Get chargers for the first time
       GetChargers();
     }, chargers);
+
+    const mergedColumns = columns.map((col) => {
+      if (!col.editable) {
+        
+        return col;
+      }
+      console.log("col", col)
+      console.log("col.dataIndex", col.dataIndex)
+      return {
+        ...col,
+        onCell: (record: RatesType) => ({
+          record,
+          inputType: col.dataIndex === 'age' ? 'number' : 'text',
+          dataIndex: col.dataIndex,
+          title: col.title,
+          editing: isEditing(record),
+        }),
+        
+      };
+    });
+
   return (
     <div>
     <Button onClick={handleAdd} style={{ marginBottom: 16 }}>Add a Charger</Button>
-    
-      <Table columns={columns} pagination={{ position: [bottom] }} dataSource={dataSource} />
+    <Form form={form} component={false}>
+      <Table 
+        components={{
+          body: {
+            cell: EditableCell,
+          },
+        }}
+        bordered
+        dataSource={dataSource}
+        columns={mergedColumns} 
+        rowClassName="editable-row"
+        pagination={{ onChange: cancel, 
+                      position: [bottom] 
+                    }}  
+                  />
+    </Form>
     </div>
   )
 }
