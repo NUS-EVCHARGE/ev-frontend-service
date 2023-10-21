9d2811bef7527f3d52f8a3390260f5e97b0303da
Song Weiyang <weiyang.song@shopee.com>
2023-10-21 13:30:00 +0800

    complete frontend booking screen

--- src/app/charger/[slug]/page.tsx
+++ src/app/charger/[slug]/page.tsx
@@ -2,11 +2,12 @@
 import { Button, Card, Col, Row } from 'antd'
 import { useRouter } from 'next/router'
 import { Dayjs } from 'dayjs';
-import React, { useState } from 'react';
-import { Calendar, theme } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Calendar, theme, Typography } from 'antd';
 import type { CalendarProps } from 'antd';
 import { Flex } from '@aws-amplify/ui-react';
 
+const { Text } = Typography
 const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
     console.log(value.format('YYYY-MM-DD'), mode);
 };
@@ -16,10 +17,11 @@ export interface Booking {
     hour: string
     min: string
     status: boolean
+    selected: boolean
 }
 
 const minInterval = ["00", "15", "30", "45"]
-const hourInterval = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "00", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"]
+const hourInterval = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "00", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
 const stubBooking: Array<Booking> = []
 
 for (var h of hourInterval) {
@@ -30,7 +32,8 @@ for (var h of hourInterval) {
                 id: id,
                 hour: h,
                 min: m,
-                status: false
+                status: false,
+                selected: false
             }
         )
         id++
@@ -38,8 +41,12 @@ for (var h of hourInterval) {
 }
 export default function ChargerBooking({ params }: { params: { slug: string } }) {
     const [selectedDate, setSelectedDate] = useState<Dayjs>()
-    const [bookingList, setBookingList] = useState([]);
+    const [bookingList, setBookingList] = useState<Booking[]>([]);
     const { token } = theme.useToken();
+
+    useEffect(() => {
+        setBookingList(stubBooking)
+    }, [])
 
     async function GetBookingByDate() {
         // get booking 
@@ -55,44 +62,58 @@ export default function ChargerBooking({ params }: { params: { slug: string } })
         borderRadius: token.borderRadiusLG,
     };
 
+    function onBookingSelected(index: number) {
+        console.log(bookingList[index].selected)
+        bookingList[index].selected = !bookingList[index].selected
+        console.log(bookingList[index].selected)
+        setBookingList(bookingList)
+    }
+
     return (
-        <Flex wrap="wrap" gap="small">
-            <div style={wrapperStyle}>
-                <Calendar fullscreen={false}
-                    onSelect={(date: Dayjs, { source }) => {
-                        if (source === 'date') {
-                            console.log('Panel Select:', date.format('YYYY-MM-DD'));
-                            setSelectedDate(date)
-                        }
-                    }} />
+        <div style={{ width: '100%' }}>
 
-            </div>
-            {selectedDate && <div style={{
-                width: '30vw',
-                maxHeight: '100%',
-                overflow: 'auto'
-            }} title="booking">
-                {Array.from(stubBooking, (b, _) => (
-                    <Row>
-                        <Col span={6}>
-                        </Col>
-                        <Col span={18}>
-                            {b.status ? <Button disabled key={b.id}>
+            <Flex wrap="wrap" gap="small">
+                <div style={wrapperStyle}>
+                    <Calendar fullscreen={false}
+                        onSelect={(date: Dayjs, { source }) => {
+                            if (source === 'date') {
+                                console.log('Panel Select:', date.format('YYYY-MM-DD'));
+                                setSelectedDate(date)
+                            }
+                        }} />
+
+                </div>
+                {selectedDate && <Card style={{
+                    margin: 'auto',
+                    width: '30vw',
+                    maxHeight: '75vh',
+                    overflow: 'scroll'
+                }} title="booking" extra={
+                    <Button >
+                        book now!
+                    </Button>}>
+                    {Array.from(bookingList, (b, index) => (
+                        <div key={index}>
+                            {b.status ? <Button style={{ width: '80%', margin: 'auto' }} disabled key={index}>
                                 Booked
-                            </Button> : <Button key={b.id} type="primary">
-                                Book now!
+                            </Button> : <Button style={{ width: '80%', margin: 'auto' }} key={index} danger={b.selected} onClick={function () {
+                                onBookingSelected(index)
+                            }}>
+                                {b.hour}:{b.min}
                             </Button>}
+                        </div>
+                    ))}
+                </Card>}
 
-                        </Col>
-                    </Row>
-                ))}
-            </div>}
-
-        </Flex>
-
+            </Flex>
+        </div>
     );
 
     // return <Card title="Booking Calendar">
 
     // </Card>
+}
+
+function BookingButton() {
+
 }
