97bb09b5a899774a1803783d59eeca4caa7c401d
Song Weiyang <weiyang.song@shopee.com>
2023-10-22 15:59:43 +0800

    intergrate create booking and show all bookings

--- src/app/charger/[slug]/page.tsx
+++ src/app/charger/[slug]/page.tsx
@@ -6,6 +6,10 @@ import React, { useEffect, useState } from 'react';
 import { Calendar, theme, Typography } from 'antd';
 import type { CalendarProps } from 'antd';
 import { Flex } from '@aws-amplify/ui-react';
+import axios from 'axios';
+import { generateFullTime, getJwtToken, isDateMatch, parseTime } from '@/app/utils';
+import { create } from 'domain';
+import { start } from 'repl';
 
 const { Text } = Typography
 const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
@@ -20,36 +24,96 @@ export interface Booking {
     selected: boolean
 }
 
+export interface CreateBookingReqObj {
+    chargerId: number
+    startTime: string
+    endTime: string
+}
+
 const minInterval = ["00", "15", "30", "45"]
 const hourInterval = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "00", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
 const stubBooking: Array<Booking> = []
+const stubBookingMap: Map<string, Booking> = new Map([])
 
 for (var h of hourInterval) {
     let id = 0
     for (var m of minInterval) {
-        stubBooking.push(
-            {
-                id: id,
-                hour: h,
-                min: m,
-                status: false,
-                selected: false
-            }
-        )
+        stubBookingMap.set(h + ":" + m, {
+            id: id,
+            hour: h,
+            min: m,
+            status: false,
+            selected: false
+        })
+        // stubBooking.push(
+        //     {
+        //         id: id,
+        //         hour: h,
+        //         min: m,
+        //         status: false,
+        //         selected: false
+        //     }
+        // )
         id++
     }
 }
-export default function ChargerBooking({ params }: { params: { slug: string } }) {
+const baseUrl = String(process.env.NEXT_PUBLIC_REACT_APP_BASE_URL)
+const getAllBookingUrl = baseUrl + "/booking/all"
+const bookingUrl = baseUrl + "/booking"
+
+export default function ChargerBooking({ params }: { params: { slug: number } }) {
     const [selectedDate, setSelectedDate] = useState<Dayjs>()
-    const [bookingList, setBookingList] = useState<Booking[]>([]);
+    const [bookingList, setBookingList] = useState<Map<string, Booking>>(new Map());
     const { token } = theme.useToken();
 
     useEffect(() => {
-        setBookingList(stubBooking)
+        GetAllBooking()
+        setBookingList(stubBookingMap)
     }, [])
 
-    async function GetBookingByDate() {
+    async function GetAllBooking() {
+        const jwtToken = await getJwtToken();
+        const { data } = await axios.get(getAllBookingUrl, {
+            headers: {
+                Accept: 'application/json',
+                Authentication: jwtToken?.toString()
+            }
+        })
+        console.log(data)
+
+        for (let b of data) {
+            if (b.Status != 'completed') {
+                let setBooking = false
+                bookingList.forEach((booking, time) => {
+
+                    if (selectedDate != undefined && isDateMatch(selectedDate, b.start_time)) {
+                        setBooking = true
+                    }
+
+                    if (selectedDate != undefined && isDateMatch(selectedDate, b.endTime)) {
+                        setBooking = false
+                    }
+
+                    booking.status = setBooking
+                    bookingList.set(time, booking)
+                })
+                setBookingList(bookingList)
+            }
+        }
         // get booking 
+    }
+
+    async function createBookingReq(bookingReq: CreateBookingReqObj) {
+        const jwtToken = await getJwtToken();
+        const { data } = await axios.post(bookingUrl, {
+            bookingReq
+        }, {
+            headers: {
+                Accept: 'application/json',
+                Authentication: jwtToken?.toString()
+            }
+        })
+        console.log(data)
     }
 
     const onDateSelect = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
@@ -62,11 +126,36 @@ export default function ChargerBooking({ params }: { params: { slug: string } })
         borderRadius: token.borderRadiusLG,
     };
 
-    function onBookingSelected(index: number) {
-        console.log(bookingList[index].selected)
-        bookingList[index].selected = !bookingList[index].selected
-        console.log(bookingList[index].selected)
+    function onBookingSelected(key: string) {
+        let b = bookingList.get(key)
+        if (b != undefined) {
+            b.selected = !b.selected
+            bookingList.set(key, b)
+        }
         setBookingList(bookingList)
+    }
+
+    function createBooking() {
+        let startTime: string
+        let endTime: string
+        let isBooking = false
+        bookingList.forEach((booking, time) => {
+            if (booking.selected && selectedDate != undefined && !isBooking) {
+                startTime = generateFullTime(selectedDate, time)
+                isBooking = true
+            }
+            if (isBooking && !booking.selected && selectedDate != undefined) {
+                endTime = generateFullTime(selectedDate, time)
+                isBooking = false
+                let bookingReq: CreateBookingReqObj = {
+                    chargerId: params.slug,
+                    startTime: startTime,
+                    endTime: endTime
+                }
+                // todo set loading function
+                createBookingReq(bookingReq)
+            }
+        });
     }
 
     return (
@@ -89,10 +178,27 @@ export default function ChargerBooking({ params }: { params: { slug: string } })
                     maxHeight: '75vh',
                     overflow: 'scroll'
                 }} title="booking" extra={
-                    <Button >
+
+                    <Button onClick={createBooking}>
                         book now!
                     </Button>}>
-                    {Array.from(bookingList, (b, index) => (
+
+                    {Array.from(bookingList.entries(), (b, index) => (
+                        <div key={index}>
+                            {b[1].status ? <Button style={{ width: '80%', margin: 'auto' }} disabled key={index}>
+                                Booked
+                            </Button> : <Button style={{ width: '80%', margin: 'auto' }} key={index} danger={b[1].selected} onClick={function () {
+                                onBookingSelected(b[0])
+                            }}>
+                                {b[0]}
+                            </Button>}
+                        </div>
+                    ))
+                    }
+
+
+
+                    {/* {Array.from(bookingList, (b, index) => (
                         <div key={index}>
                             {b.status ? <Button style={{ width: '80%', margin: 'auto' }} disabled key={index}>
                                 Booked
@@ -102,7 +208,9 @@ export default function ChargerBooking({ params }: { params: { slug: string } })
                                 {b.hour}:{b.min}
                             </Button>}
                         </div>
-                    ))}
+                    ))
+
+                    } */}
                 </Card>}
 
             </Flex>
--- src/app/utils/index.tsx
+++ src/app/utils/index.tsx
@@ -1,15 +1,33 @@
 import { Auth } from "aws-amplify";
+import { Dayjs } from 'dayjs';
 
-async function getJwtToken() : Promise<string | void>{
+async function getJwtToken(): Promise<string | void> {
   if (!Auth || !Auth.currentAuthenticatedUser) {
     return;
   }
   return Auth.currentSession().then((data) => {
     console.log(data.getIdToken().getJwtToken());
     return data.getIdToken().getJwtToken();
-  }). catch (err => {
+  }).catch(err => {
     console.log(err);
   });
 }
 
-export { getJwtToken };
+// parse time in the format 2023-10-28T16:35:48Z
+function parseTime(time: string): [string, string, string, string] {
+  const [wholeDate, wholeTime] = time.split("T")
+  const [day, month, year] = wholeDate.split("-")
+  const [parsedTime, _] = wholeTime.split("Z")
+  return [day, month, year, parsedTime]
+}
+
+// parse time in the format 2023-10-28T16:35:48Z and check with dayjs
+function isDateMatch(date: Dayjs, time: string): Boolean {
+  const [day, month, year, parsedTime] = parseTime(time)
+  let compareDay = date.day.toString()
+  let compareMonth = date.month.toString()
+  let compareYear = date.year.toString()
+  console.log(compareDay, compareMonth, compareYear, time)
+  return compareDay == day && compareMonth == month && compareYear == year
+}
+export { getJwtToken, parseTime, isDateMatch };
